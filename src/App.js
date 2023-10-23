537181f668f733430877c62a7aa7772abc28e2e6
kishorekuraganti <krishnakishore9347@gmail.com>
2023-10-23 16:31:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import ColorPicker from "./ColorPicker";
+import "./Colorpicker.css";
+const App = () => {
+  const colors = [
+    "red",
+    "lime",
+    "blue",
+    "yellow",
+    "magenta",
+    "cyan",
+    "orange",
+    "purple",
+    "pink",
+    "green",
+    "tomato",
+    "turquoise",
+    "brown",
+    "orange",
+    "steelblue",
+    "gold",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div className="layer1">
+        <h1>Color Picker</h1>
+        <div className="layer2">
+          <div id="id1">
+            <ColorPicker colors={colors} />
+          </div>
+        </div>
+      </div>
+    </center>
   );
-}
-
+};
 export default App;
